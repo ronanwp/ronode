cdf5b872a41f7d7fe4c33dba99118c68072b8fd2
ronanwp <ronan@wattpad.com>
2022-02-07 10:31:58 -0400

    Testing deploy via github

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
+        message: 'Updated Hello World and Deployed via github!',
         input: event,
       },
       null,
